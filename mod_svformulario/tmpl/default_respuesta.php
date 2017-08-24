1e09152da358c07a1db70de1f81437d1184d7a06
Ricardo Oficina <ricardolau6@gmail.com>
2017-08-24 02:21:17 +0200

    Se premaqueta la salida de error

--- mod_svformulario/tmpl/default_respuesta.php
+++ mod_svformulario/tmpl/default_respuesta.php
@@ -18,21 +18,29 @@ defined('_JEXEC') or die('Restricted access');
 <div id="Svformulario">
  <p><?php 
 	echo ' <h1> Hubo un error , no se envio</h1>';
-	echo '<pre>';
+	//~ echo '<pre>';
 	//~ print_r($svform);
-	echo 'Respuesta de filtro:<br/>';
-	print_r($res_filtro);
-	echo '<br/>';
+	if (isset($datos['error'])) {
+		echo 'Error en campos:<br/>';
+		echo '<ul>';
+		foreach ($datos['error'] as $mensaje) {
+			echo '<li>'.$mensaje."</li>";
+		}
+		echo '</ul>';
+	} else if (isset($res_filtro)) {
+		echo 'Respuesta de filtro:<br/>';
+		echo "<ul><li>".$res_filtro."</li></ul>";
+	}
+	//~ print_r($datos['error']);
 	//~ echo 'Datos a enviar :<br/>';
 	//~ print_r($datos);
 	//~ echo 'Respuesta Envio:<br/>';
 	//~ if (isset($res_envio)){
 	//~ print_r($res_envio);
 	//~ }
-	echo '</pre>';
+	//~ echo '</pre>';
 	
 	?>
 </p>
 
 </div>
-      
